722626eaa74b21c4751f678c95a2487f43f0bacd
ninjatill <ninjatill@yahoo.com>
2026-06-01 16:26:34 -0400

    I2C address file tweak.
    Just reorganizing I2C address file.

--- firmware/pcb_touch_synth/config/i2c_addresses.h
+++ firmware/pcb_touch_synth/config/i2c_addresses.h
@@ -15,13 +15,12 @@ constexpr uint8_t I2C_ADDR_TOUCH_2  = 0x21;
 // I2C1 DEVICES
 // ======================================================
 
+// Audio Amp/DAC
+constexpr uint8_t I2C_ADDR_AUDIO_DAC = 0x18;   // TLV320DAC3100
+
 // Power Controller/Battery Charger
-// TODO: verify MP2724 address from datasheet / schematic
 constexpr uint8_t I2C_ADDR_MP2724 = 0x3F;
 
 // LED Drivers
 constexpr uint8_t I2C_ADDR_LED_1  = 0x40;    // PCA9685, A0=GND
 constexpr uint8_t I2C_ADDR_LED_2  = 0x41;    // PCA9685, A0=3V3
-
-// Audio Amp/DAC
-constexpr uint8_t I2C_ADDR_AUDIO_DAC = 0x18;   // TLV320DAC3100
